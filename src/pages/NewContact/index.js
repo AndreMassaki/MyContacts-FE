45889be9c67655ab8e0db4b405591a6b81d3c56f
AndreMassaki <andremassaki2004@gmail.com>
2022-12-29 17:06:55 -0300

    creating the Button component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import PageHeader from '../../components/PageHeader';
 
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -15,7 +16,17 @@ export default function NewContact() {
       />
       <Select>
         <option value="instagram">Instagram</option>
+        <option value="instagram">Instagram</option>
+        <option value="instagram">Instagram</option>
+        <option value="instagram">Instagram</option>
+        <option value="instagram">Instagram</option>
       </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
     </>
   );
 }
