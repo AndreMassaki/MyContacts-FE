834c6af9858f6c0a789bec32a947e2121d06dace
AndreMassaki <andremassaki2004@gmail.com>
2022-12-31 19:50:52 -0300

    displaying the errors in the interface

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -49,7 +49,9 @@ export default function ContactForm({ buttonLabel }) {
     }
   }
 
-  console.log(errors);
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -61,19 +63,25 @@ export default function ContactForm({ buttonLabel }) {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormGroup>
+      <FormGroup
+        error={getErrorMessageByFieldName('name')}
+      >
         <Input
           placeholder="Nome"
           value={name}
           onChange={handleNameChange}
+          error={getErrorMessageByFieldName('name')}
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup
+        error={getErrorMessageByFieldName('email')}
+      >
         <Input
           placeholder="E-mail"
           value={email}
           onChange={handleEmailChange}
+          error={getErrorMessageByFieldName('email')}
         />
       </FormGroup>
 
