517678016bd5611e83af7549ae6382d88dbacd8f
AndreMassaki <andremassaki2004@gmail.com>
2023-01-01 17:29:24 -0300

    simple style changes

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -48,7 +48,7 @@ export default function ContactForm({ buttonLabel }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} noValidate>
       <FormGroup
         error={getErrorMessageByFieldName('name')}
       >
@@ -64,6 +64,7 @@ export default function ContactForm({ buttonLabel }) {
         error={getErrorMessageByFieldName('email')}
       >
         <Input
+          type="email"
           placeholder="E-mail"
           value={email}
           onChange={handleEmailChange}
